25d828e2d3787593a1365c0c86b76b6809046a46
Julius Ware <jcware8@gmail.com>
2024-10-08 20:23:18 -0400

    adding props

---
+++ src/ProfileCard.js
@@ -0,0 +1,12 @@
+function ProfileCard ({title, handle}) {
+// const { title, hanle } = props;
+	
+	return (
+		<div>
+			<div> Title is {title} </div>
+			<div> Handle is {handle} </div>
+		</div>
+	);
+}
+
+export default ProfileCard;
