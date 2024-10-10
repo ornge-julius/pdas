1d53a9ecb743a971064efbc2099a832378c317b0
Julius Ware <jcware@covermymeds.com>
2024-10-10 12:06:16 -0400

    Add bulma and stying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 
 function App() {
 	return (
     <div>
       <div> Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99"/>
-      <ProfileCard title="Cortana" handle="@cortana99"/>
-      <ProfileCard title="Siri" handle="@siri99"/>
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage}/>
+            </div>
+
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri99" image={SiriImage}/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,20 @@
-function ProfileCard ({title, handle}) {
-// const { title, hanle } = props;
+function ProfileCard ({title, handle, image}) {
+// const { title, handle } = props;
 	
 	return (
-		<div>
-			<div> Title is {title} </div>
-			<div> Handle is {handle} </div>
+		<div className="card">
+			<div className="card-image">
+				<figure className="image is-1by1">
+					<img src={image} alt=''/>
+				</figure>
+			</div>
+
+			<div className="card-content">
+				<div className="media-content">
+					<p className="title is-4">{title}</p>
+					<p className="subtitle is-6">{handle}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
